937fb76d2f92a4b06a15b14a11d80f4d3528cbdb
yjm <wwoals14@naver.com>
2023-05-25 14:55:34 +0900

    프로필

--- js/custom.js
+++ js/custom.js
@@ -81,6 +81,11 @@ $(window).on("scroll",function(){
  });
 
 });
+$('.logo').click(function(e){
+    e.preventDefault();
+     //상단으로 스르륵 이동합니다.
+    $("html,body").stop().animate({"scrollTop":0},1000); 
+ });
 
 });
 
